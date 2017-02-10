e187950bbd8e95e5815c5fc7cc1eed3bed0780f8
andrew <gumenniy.oik@gmail.com>
2017-02-10 23:19:14 +0200

    gh-master

--- app/components/producer/producerController.js
+++ app/components/producer/producerController.js
@@ -1,18 +1,33 @@
 theatreSite.controller('producerController', 
 	['$scope','$http',function($scope, $http) {
 	
-    $http.get("app/components/producer/json/maininfo.json").then(function(response) {
-    	
-      $scope.maininfo = response.data.maininfo;
-	  
-	  console.log($scope.maininfo.photoUrl);
-
-    });	
-   $http.get("app/components/producer/json/photos.json").then(function(response) {
+    $http.get("app/components/producer/json/photos.json").then(function(response) {
     	
       $scope.photos = response.data.photos;
 	  
 	  
 
     });	
-}]);
+   window.onscroll = function() {
+       
+        if($(window).scrollTop()!=0){
+document.querySelector(".header").style.background = "black";
+}
+else{
+ document.querySelector(".header").style.background = "rgba(0, 0, 0, 0.4)"; 
+}
+ if(document.querySelector(".fa-chevron-down")){
+          document.querySelector(".fa-chevron-down").style.opacity = "0";
+        }
+      }
+   $http.get("app/components/producer/json/plays.json").then(function(response) {
+    	
+      $scope.plays = response.data.plays;
+	  
+
+    });	
+}]).filter('toTrusted', function ($sce) {
+    return function (value) {
+        return $sce.trustAsHtml(value);
+    };
+}); 
